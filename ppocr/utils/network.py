ea7d73195216263122e9632d86bd3f82c568ce23
zhongzhengguan <zhongzhengguan@sensetime.com>
2023-11-08 15:46:18 +0800

    推理模型支持文件名：model和inference，与infer/utility逻辑一致

--- ppocr/utils/network.py
+++ ppocr/utils/network.py
@@ -44,29 +44,33 @@ def download_with_progressbar(url, save_path):
 def maybe_download(model_storage_directory, url):
     # using custom model
     tar_file_name_list = ['.pdiparams', '.pdiparams.info', '.pdmodel']
-    if not os.path.exists(
-            os.path.join(model_storage_directory, 'inference.pdiparams')
-    ) or not os.path.exists(
-            os.path.join(model_storage_directory, 'inference.pdmodel')):
-        assert url.endswith('.tar'), 'Only supports tar compressed package'
-        tmp_path = os.path.join(model_storage_directory, url.split('/')[-1])
-        print('download {} to {}'.format(url, tmp_path))
-        os.makedirs(model_storage_directory, exist_ok=True)
-        download_with_progressbar(url, tmp_path)
-        with tarfile.open(tmp_path, 'r') as tarObj:
-            for member in tarObj.getmembers():
-                filename = None
-                for tar_file_name in tar_file_name_list:
-                    if member.name.endswith(tar_file_name):
-                        filename = 'inference' + tar_file_name
-                if filename is None:
-                    continue
-                file = tarObj.extractfile(member)
-                with open(
-                        os.path.join(model_storage_directory, filename),
-                        'wb') as f:
-                    f.write(file.read())
-        os.remove(tmp_path)
+    file_names = ['model', 'inference']
+    for file_name in file_names:
+        if os.path.exists(os.path.join(model_storage_directory, file_name + '.pdiparams')) \
+                and os.path.exists(os.path.join(model_storage_directory, file_name + '.pdmodel')):
+            # 推理模型已经存在，则不用下载
+            return
+
+    # 下载推理模型文件，仅支持tar文件
+    assert url.endswith('.tar'), 'Only supports tar compressed package'
+    tmp_path = os.path.join(model_storage_directory, url.split('/')[-1])
+    print('download {} to {}'.format(url, tmp_path))
+    os.makedirs(model_storage_directory, exist_ok=True)
+    download_with_progressbar(url, tmp_path)
+    with tarfile.open(tmp_path, 'r') as tarObj:
+        for member in tarObj.getmembers():
+            filename = None
+            for tar_file_name in tar_file_name_list:
+                if member.name.endswith(tar_file_name):
+                    filename = 'inference' + tar_file_name
+            if filename is None:
+                continue
+            file = tarObj.extractfile(member)
+            with open(
+                    os.path.join(model_storage_directory, filename),
+                    'wb') as f:
+                f.write(file.read())
+    os.remove(tmp_path)
 
 
 def maybe_download_params(model_path):
