877c349fd401add183a8157e3d86b2a6e1e88d69
zhongzhengguan <zhongzhengguan@sensetime.com>
2023-11-15 14:02:56 +0800

    动态适配：若CUDA设备不存在，则不启用GPU

--- tools/infer/utility.py
+++ tools/infer/utility.py
@@ -661,7 +661,7 @@ def get_minarea_rect_crop(img, points):
 
 def check_gpu(use_gpu):
     if use_gpu \
-            and not (paddle.is_compiled_with_cuda() and len(paddle.fluid.cuda_places()) == 0):
+            and not (paddle.is_compiled_with_cuda() and len(paddle.fluid.cuda_places()) > 0):
         # 动态适配：若CUDA设备不存在，则不启用GPU
         use_gpu = False
     return use_gpu
